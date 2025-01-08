3064f9bc5c787e72a5eab41b0aea56af67539af6
isybliss <isyblissumunna@gmail.com>
2025-01-08 08:19:00 +0100

    updated the JS file, wrote the function to check user input is a valid number that is greater than or equal to 0

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+    const inputs = [];
+    const quotients = [];
+    const remainders = [];
+};
+
+const checkUserInput = () => {
+    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+        alert("Please provide a decimal number greater than or equal to 0");
+        return;
+    }
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
+};
+
+convertBtn.addEventListener('click', checkUserInput);
+numberInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        checkUserInput();
+    }
+})
+
+
+//NOTES
+/**
+ * falsy values equate to false and include: undefined, null, 0 and empty string "".
+ * instead of checking if a value equal a falsy value, use the logical NOT operator "!" to check if the value itself is falsy
+ 
+        const num = 0;
+        console.log(num === 0); // true
+        console.log(!num); // true
+
+ * e is a common parameter name for the event object
+
+ * parseInt() is used to check and normalize numbers in JavaScript.
+ * it converts a string into an integer or whole number or Nan(Not a Number).
+ * 
+ * isNaN() is used to check if the string or number taken as an argument is a number or not.
+ * it returns true if it is not a number
+ */
