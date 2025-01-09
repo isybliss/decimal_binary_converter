f24ea214840179e7094e204973ec55f5bc5ea731
isybliss <isyblissumunna@gmail.com>
2025-01-09 10:00:11 +0100

    built the recursion function

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+//Call stack example
+/*
+const countDownAndUp = (number) => {
+    console.log(number);
+    if (number === 0) {
+        console.log("Reached base case");
+        return;
+    } else {
+        countDownAndUp(number - 1);
+        console.log(number);
+    }   
+};
 
+countDownAndUp(3);
+*/
 const decimalToBinary = (input) => {
+    if (input === 0) {
+        return "0";
+    } else {
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+    }
+    /*
     const inputs = [];
     const quotients = [];
     const remainders = [];
+
+    if (input === 0) {
+        result.innerText = "0";
+        return;
+    }
+    
+    while (input > 0) {
+        const quotient = Math.floor(input / 2);
+        const remainder = input % 2;
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
+        input = quotient;
+    }
+
+    console.log("Inputs: ", inputs);
+    console.log("Quotients: ", quotients);
+    console.log("Remainders: ", remainders);
+    result.innerText = remainders.reverse().join("");
+    */
+   /*
+   let binary = "";
+
+   if (input === 0) {
+    binary = "0";
+   }
+
+   while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+   }
+   result.innerText = binary;
+   */
 };
 
 const checkUserInput = () => {
@@ -22,7 +75,7 @@ numberInput.addEventListener("keydown", (e) => {
     if(e.key === "Enter") {
         checkUserInput();
     }
-})
+});
 
 
 //NOTES
